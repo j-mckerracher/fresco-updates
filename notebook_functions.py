7a02bf434eca09b7efc751c37f69829b5da792d4
josh <jmckerra@purdue.edu>
2023-07-02 15:44:06 -0600

    Saving work

--- notebook_functions.py
+++ notebook_functions.py
@@ -414,16 +414,46 @@ def create_excel_download_link(df, title=None, filename="data.xlsx"):
 
 
 # -------------- CELL 7 --------------
-def get_average():
+# Aryamaan
+def get_average() -> pd.DataFrame:
     pass
 
 
+# Aryamaan
 def get_mean():
     pass
 
 
-def get_median():
-    pass
+def get_median(time_series: pd.DataFrame, rolling=False, window=None) -> pd.DataFrame:
+    """
+    Calculates the median of the provided time_series DataFrame, either on the entire DataFrame or on a rolling window
+    basis.
+
+    Parameters:
+    :param time_series: A pandas DataFrame that contains a column 'Value'. The median is calculated on the 'Value'
+                        column.
+    :param rolling: A boolean indicating whether the median should be calculated for the entire DataFrame (False) or
+                    on a rolling window basis (True).
+    :param window: The size of the rolling window for which the median is to be calculated. It should be specified
+                   in string format like use D for days, H for hours, T for minutes, and S for seconds. This parameter
+                   is considered only if rolling is set to True.
+
+    Returns:
+    :return: A pandas DataFrame or Series which contains the median of the 'Value' column of the provided time_series
+             DataFrame. If rolling is set to True, the result will be a DataFrame with the rolling window median. If
+             rolling is False, the result will be a Series with the overall median.
+    """
+    # drop columns not needed
+    time_series.drop(['Job Id', 'Host', 'Event', 'Units'], axis=1, inplace=True)
+
+    if rolling:
+        return time_series.rolling(window=window).median()
+
+    return time_series.median()
+
+
+
+
 
 
 def get_standard_deviation():
